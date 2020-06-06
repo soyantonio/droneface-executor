f81cb8688094c2b884075d61831048932c9e9796
Jesus Antonio <antonio@almavios.com>
2020-06-06 17:46:34 -0500

    Change api endpoint from service to services

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import services.data as services
 app = Flask(__name__)
 
 
-@app.route('/api/service/<service_id>')
+@app.route('/api/services/<service_id>')
 def hello_world(service_id):
     if services.is_not_present(service_id):
         abort(404)
